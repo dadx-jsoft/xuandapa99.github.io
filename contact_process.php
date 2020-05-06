499ae246cf691678cbbc7f9161916997fe9cad0e
Duong Xuan Da <xuandapa@gmail.com>
2020-05-06 22:14:59 +0700

    modify contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "xuandapa@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
@@ -11,7 +11,7 @@
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
     $subject = "You have a message from your Bitmap Photography.";
 
